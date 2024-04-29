72de3ac082b8a1691a8283d915230ee62982f430
mroscardev <mroscardev@gmail.com>
2024-04-29 19:25:48 +0200

    model, migration user_details + suppliers + transactions

---
+++ database/migrations/2024_04_29_171904_create_user_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_user');
+            $table->string('name');
+            $table->string('surname');
+            $table->string('dni', 10);
+            $table->string('address'); 
+            $table->text('avatar')->nullable();
+            $table->timestamps();$table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_details');
+    }
+};
